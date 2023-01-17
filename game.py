2a21b66706e4c7a85c8231db558772c146435ba1
MathewLaws <93098734+MathewLaws@users.noreply.github.com>
2023-01-17 22:07:46 +0000

    V1
    Added more piece functionality

--- game.py
+++ game.py
@@ -18,50 +18,50 @@ board_rect = board_img.get_rect()
 
 board = [[0 for _ in range(8)] for _ in range(8)]
 selected = 0
-turn = "w"
+turn = True
 
-board[0][0] = Rook(0, 0, "b")
-board[0][1] = Knight(1, 0, "b")
-board[0][2] = Bishop(2, 0, "b")
-board[0][3] = Queen(3, 0, "b")
-board[0][4] = King(4, 0, "b")
-board[0][5] = Bishop(5, 0, "b")
-board[0][6] = Knight(6, 0, "b")
-board[0][7] = Rook(7, 0, "b")
+board[0][0] = Rook(0, 0, False)
+board[0][1] = Knight(1, 0, False)
+board[0][2] = Bishop(2, 0, False)
+board[0][3] = Queen(3, 0, False)
+board[0][4] = King(4, 0, False)
+board[0][5] = Bishop(5, 0, False)
+board[0][6] = Knight(6, 0, False)
+board[0][7] = Rook(7, 0, False)
 
-board[6][0] = Pawn(0, 6, "w")
-board[6][1] = Pawn(1, 6, "w")
-board[6][2] = Pawn(2, 6, "w")
-board[6][3] = Pawn(3, 6, "w")
-board[6][4] = Pawn(4, 6, "w")
-board[6][5] = Pawn(5, 6, "w")
-board[6][6] = Pawn(6, 6, "w")
-board[6][7] = Pawn(7, 6, "w")
+board[6][0] = Pawn(0, 6, True)
+board[6][1] = Pawn(1, 6, True)
+board[6][2] = Pawn(2, 6, True)
+board[6][3] = Pawn(3, 6, True)
+board[6][4] = Pawn(4, 6, True)
+board[6][5] = Pawn(5, 6, True)
+board[6][6] = Pawn(6, 6, True)
+board[6][7] = Pawn(7, 6, True)
 
-board[7][0] = Rook(0, 7, "w")
-board[7][1] = Knight(1, 7, "w")
-board[7][2] = Bishop(2, 7, "w")
-board[7][3] = Queen(3, 7, "w")
-board[7][4] = King(4, 7, "w")
-board[7][5] = Bishop(5, 7, "w")
-board[7][6] = Knight(6, 7, "w")
-board[7][7] = Rook(7, 7, "w")
+board[7][0] = Rook(0, 7, True)
+board[7][1] = Knight(1, 7, True)
+board[7][2] = Bishop(2, 7, True)
+board[7][3] = Queen(3, 7, True)
+board[7][4] = King(4, 7, True)
+board[7][5] = Bishop(5, 7, True)
+board[7][6] = Knight(6, 7, True)
+board[7][7] = Rook(7, 7, True)
 
-board[1][0] = Pawn(0, 1, "b")
-board[1][1] = Pawn(1, 1, "b")
-board[1][2] = Pawn(2, 1, "b")
-board[1][3] = Pawn(3, 1, "b")
-board[1][4] = Pawn(4, 1, "b")
-board[1][5] = Pawn(5, 1, "b")
-board[1][6] = Pawn(6, 1, "b")
-board[1][7] = Pawn(7, 1, "b")
+board[1][0] = Pawn(0, 1, False)
+board[1][1] = Pawn(1, 1, False)
+board[1][2] = Pawn(2, 1, False)
+board[1][3] = Pawn(3, 1, False)
+board[1][4] = Pawn(4, 1, False)
+board[1][5] = Pawn(5, 1, False)
+board[1][6] = Pawn(6, 1, False)
+board[1][7] = Pawn(7, 1, False)
 
 
 def draw():
     for i in range(8):
         for j in range(8):
             if board[i][j] != 0:
-                board[i][j].draw(screen)
+                board[i][j].draw(board, screen)
 
 
 def click(pos):
@@ -88,10 +88,11 @@ def select(index):
 
 
 def move(index):
-    board[index[0]][index[1]] = board[selected.y][selected.x]
-    board[selected.y][selected.x].x = index[1]
-    board[selected.y][selected.x].y = index[0]
-    board[selected.y][selected.x].selected = False
+    piece = board[selected.y][selected.x]
+    board[index[0]][index[1]] = piece
+    piece.x = index[1]
+    piece.y = index[0]
+    piece.selected = False
 
 
 while True:
@@ -102,12 +103,11 @@ while True:
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             index = click(pos)
-            # change
             if selected != 0:
                 if [index[1], index[0]] in selected.valid_moves(board):
                     move(index)
                     selected = 0
-                    turn = "b"
+                    turn = not turn
 
             if board[index[0]][index[1]] != 0:
                 if board[index[0]][index[1]].team == turn:
--- piece.py
+++ piece.py
@@ -27,12 +27,11 @@ class Piece:
         self.team = team
         self.selected = False
 
-    def draw(self, screen):
+    def draw(self, board, screen):
         x_pos = self.x * (512/8)
         y_pos = self.y * (512/8)
 
-        # draw this
-        if self.team == "w":
+        if self.team:
             g = W[self.img]
         else:
             g = B[self.img]
@@ -42,6 +41,9 @@ class Piece:
         if self.selected:
             pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(
                 (self.x*(512/8)), (self.y*(512/8)), (512/8), (512/8)), 2)
+            for i in self.valid_moves(board):
+                pygame.draw.ellipse(screen, (255, 0, 0), pygame.Rect(
+                    ((i[0]*(512/8))+(512/8)/2-10), ((i[1]*(512/8))+(512/8)/2-10), 20, 20))
 
     def isSelected(self):
         return self.selected
@@ -53,9 +55,28 @@ class Pawn(Piece):
     def valid_moves(self, board):
         moves = []
 
-        if self.team == "w":
+        if self.team:
             moves.append([self.x, (self.y - 1)])
             moves.append([self.x, (self.y - 2)])
+            if self.x-1 >= 0:
+                if board[self.y-1][self.x-1] != 0:
+                    if board[self.y-1][self.x-1].team != self.team:
+                        moves.append([self.x-1, self.y-1])
+            if self.x+1 <= 7:
+                if board[self.y-1][self.x+1] != 0:
+                    if board[self.y-1][self.x+1].team != self.team:
+                        moves.append([self.x+1, self.y-1])
+        else:
+            moves.append([self.x, (self.y + 1)])
+            moves.append([self.x, (self.y + 2)])
+            if self.x-1 >= 0:
+                if board[self.y+1][self.x-1] != 0:
+                    if board[self.y+1][self.x-1].team != self.team:
+                        moves.append([self.x-1, self.y+1])
+            if self.x+1 <= 7:
+                if board[self.y+1][self.x+1] != 0:
+                    if board[self.y+1][self.x+1].team != self.team:
+                        moves.append([self.x+1, self.y+1])
 
         return moves
 
@@ -64,32 +85,137 @@ class Bishop(Piece):
     img = 1
 
     def valid_moves(self, board):
-        pass
+        moves = []
+
+        for i in range(1, 8):
+            if self.y-1*i >= 0 and self.x-1*i >= 0:
+                if board[self.y-1*i][self.x-1*i] != 0:
+                    if board[self.y-1*i][self.x-1*i].team != self.team:
+                        moves.append([self.x-1*i, self.y-1*i])
+                else:
+                    moves.append([self.x-1*i, self.y-1*i])
+            if self.y-1*i >= 0 and self.x+1*i <= 7:
+                if board[self.y-1*i][self.x+1*i] != 0:
+                    if board[self.y-1*i][self.x+1*i].team != self.team:
+                        moves.append([self.x+1*i, self.y-1*i])
+                else:
+                    moves.append([self.x+1*i, self.y-1*i])
+            if self.y+1*i <= 7 and self.x+1*i <= 7:
+                if board[self.y+1*i][self.x+1*i] != 0:
+                    if board[self.y+1*i][self.x+1*i].team != self.team:
+                        moves.append([self.x+1*i, self.y+1*i])
+                else:
+                    moves.append([self.x+1*i, self.y+1*i])
+            if self.y+1*i <= 7 and self.x-1*i >= 0:
+                if board[self.y+1*i][self.x-1*i] != 0:
+                    if board[self.y+1*i][self.x-1*i].team != self.team:
+                        moves.append([self.x-1*i, self.y+1*i])
+                else:
+                    moves.append([self.x-1*i, self.y+1*i])
+
+        return moves
 
 
 class Knight(Piece):
     img = 2
 
     def valid_moves(self, board):
-        pass
+        moves = []
+        X = [2, 1, -1, -2, -2, -1, 1, 2]
+        Y = [1, 2, 2, 1, -1, -2, -2, -1]
+
+        for i in range(len(X)):
+            if self.x+X[i] >= 0 and self.x+X[i] <= 7 and self.y+Y[i] >= 0 and self.y+Y[i] <= 7:
+                if board[self.y+Y[i]][self.x+X[i]] != 0:
+                    if board[self.y+Y[i]][self.x+X[i]].team != self.team:
+                        moves.append([self.x+X[i], self.y+Y[i]])
+                else:
+                    moves.append([self.x+X[i], self.y+Y[i]])
+
+        return moves
 
 
 class Rook(Piece):
     img = 3
 
     def valid_moves(self, board):
-        pass
+        moves = []
+        for i in range(7):
+            if i != self.y:
+                if board[i][self.x] != 0:
+                    if board[i][self.x].team != self.team:
+                        moves.append([self.x, i])
+                else:
+                    moves.append([self.x, i])
+        for i in range(7):
+            if i != self.x:
+                if board[self.y][i] != 0:
+                    if board[self.y][i].team != self.team:
+                        moves.append([i, self.y])
+                else:
+                    moves.append([i, self.y])
+        return moves
 
 
 class Queen(Piece):
     img = 4
 
     def valid_moves(self, board):
-        pass
+        moves = []
+        # up/down
+        for i in range(7):
+            if i != self.y:
+                if board[i][self.x] != 0:
+                    if board[i][self.x].team != self.team:
+                        moves.append([self.x, i])
+                else:
+                    moves.append([self.x, i])
+        for i in range(7):
+            if i != self.x:
+                if board[self.y][i] != 0:
+                    if board[self.y][i].team != self.team:
+                        moves.append([i, self.y])
+                else:
+                    moves.append([i, self.y])
+        # diagnals
+
+        for i in range(1, 8):
+            if self.y-1*i >= 0 and self.x-1*i >= 0:
+                if board[self.y-1*i][self.x-1*i] != 0:
+                    if board[self.y-1*i][self.x-1*i].team != self.team:
+                        moves.append([self.x-1*i, self.y-1*i])
+                else:
+                    moves.append([self.x-1*i, self.y-1*i])
+            if self.y-1*i >= 0 and self.x+1*i <= 7:
+                if board[self.y-1*i][self.x+1*i] != 0:
+                    if board[self.y-1*i][self.x+1*i].team != self.team:
+                        moves.append([self.x+1*i, self.y-1*i])
+                else:
+                    moves.append([self.x+1*i, self.y-1*i])
+            if self.y+1*i <= 7 and self.x+1*i <= 7:
+                if board[self.y+1*i][self.x+1*i] != 0:
+                    if board[self.y+1*i][self.x+1*i].team != self.team:
+                        moves.append([self.x+1*i, self.y+1*i])
+                else:
+                    moves.append([self.x+1*i, self.y+1*i])
+            if self.y+1*i <= 7 and self.x-1*i >= 0:
+                if board[self.y+1*i][self.x-1*i] != 0:
+                    if board[self.y+1*i][self.x-1*i].team != self.team:
+                        moves.append([self.x-1*i, self.y+1*i])
+                else:
+                    moves.append([self.x-1*i, self.y+1*i])
+
+        return moves
 
 
 class King(Piece):
     img = 5
 
     def valid_moves(self, board):
-        pass
+        moves = []
+        for i in range(-1, 1):
+            for j in range(-1, 1):
+                if i == 0 and j == 0:
+                    pass
+                moves.append([self.x+j, self.y+i])
+        return moves
